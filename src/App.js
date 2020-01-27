fdd2ae634aa781daf8317bf6118bdfa6d0183d0a
ketycossey <54608435+ketycossey@users.noreply.github.com>
2020-01-27 15:28:05 -0600

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
+import logo from "./logo.svg";
 import "./App.css";
-import Welcome from "./Welcome";
+import Counter from "./Counter";
 
 class App extends Component {
   render() {
     return (
-      <>
-        <Welcome />
-      </>
+      <div>
+        <Counter />
+      </div>
     );
   }
 }
-
 export default App;
